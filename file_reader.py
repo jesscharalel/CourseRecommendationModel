c41bbd62609934a73e527d765a2e711c56afdd52
catherine ting <catherine.ting@tufts.edu>
2026-04-14 15:03:24 -0400

    uploaded file that converts txt to py dataframe

---
+++ file_reader.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+import csv
+
+# read txt file
+with open("CourseText.txt", 'r') as f:
+    lines = f.readlines()
+
+# create one column called department
+courses = pd.DataFrame(lines, columns=["Department"])
+
+# split department into department and ID by the -
+courses[['Department', 'ID']] = courses['Department'].str.split("-", n=1, expand = True)
+
+#split ID into ID and Title by the first space
+courses[['ID', 'Title']] = courses['ID'].str.split(" ", n=1, expand = True)
+
+# remove the \n in each column
+courses['Title'] = courses['Title'].str.strip("\n")
+
+print(courses)
